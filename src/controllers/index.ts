893616ed9f2ae409c7db3c347da6f6d1fba8237f
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2024-04-14 15:42:04 +0530

    Code restructure:

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 
-export const handlePostRequest = 
-(req: Request<{world: string}, {}, {name: string, age: number, city: string}>, res: Response, next: NextFunction) => {
+type CustomRequest = Request<{world: string}, {}, {name: string, age: number, city: string}>
+export const handlePostRequest = (req: CustomRequest , res: Response, next: NextFunction) => {
     console.log(`Hello ${req.params.world}`);
     res.json(req.body.name);
 }
