ad4ca31e7fe853bb7a6e63091c52609b75adcedc
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2024-04-15 14:23:36 +0530

    morgan logger added

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -5,10 +5,12 @@ import bodyParser from "body-parser";
 import { sessionLoader } from "./session";
 import { routesLoader } from "./routes";
 import errorHandler from "../middleware/errors";
+import morgan from 'morgan';
 
 const appLoader = (app: Express) => {
     app.use(helmet());
     app.use(bodyParser.json());
+    app.use(morgan('dev'))
     
     sessionLoader(app);
     routesLoader(app);
