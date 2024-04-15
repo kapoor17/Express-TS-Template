3510064bae762ce0620d16cd008b4aff0fc40782
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2024-04-15 22:10:59 +0530

    external express-session store with mongodb

--- src/loaders/session.ts
+++ src/loaders/session.ts
@@ -1,5 +1,7 @@
 import {Express} from 'express';
 import session, {SessionOptions} from "express-session";
+import mongoose from "mongoose";
+import MongoStore from "connect-mongo";
 declare module "express-session" {
     interface SessionData {
         userId: string,
@@ -8,17 +10,28 @@ declare module "express-session" {
 }
 
 export const sessionLoader = (app: Express) => {
+    let sessionStore;
+    try{
+        sessionStore = MongoStore.create({
+            mongoUrl: process.env.MONGO_SESSION_STORE_URI || ""
+        })
+    }catch(e){
+        console.error(`Error while connecting to Mongo Session Store: ${e}`);
+        throw e;
+    }
+
     const sessionObject: SessionOptions = {
         secret: process.env.SESSION_SECRET || "",
         cookie: { maxAge: 1000*60*60*24 },
         resave: false,
         saveUninitialized: false,
-        store: new session.MemoryStore()
+        store: sessionStore
     }
     // secure needs an https connection, that is why it was giving new session tds with every request
     if(app.get('env') == 'production' && sessionObject.cookie){
         app.set('trust proxy', 1);
         sessionObject.cookie.secure = true
     }
+    
     app.use(session(sessionObject));
 }
